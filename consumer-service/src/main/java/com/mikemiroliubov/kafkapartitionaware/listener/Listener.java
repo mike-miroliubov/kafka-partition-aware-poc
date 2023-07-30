4623eec58ac15b8e69d3234221e8029cb2d33386
Mikhail Miroliubov <Mikhail_Miroliubov@epam.com>
2023-07-30 00:32:46 -0400

    PoC with static partitions

--- consumer-service/src/main/java/com/mikemiroliubov/kafkapartitionaware/listener/Listener.java
+++ consumer-service/src/main/java/com/mikemiroliubov/kafkapartitionaware/listener/Listener.java
@@ -2,12 +2,16 @@ package com.mikemiroliubov.kafkapartitionaware.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class Listener {
-    @KafkaListener(topics = "results")
+    @KafkaListener(topicPartitions = {
+            @TopicPartition(topic = "results", partitions = "${partition}")
+    })
     public void receiveMessage(String data) {
         log.info("{}", data);
     }
